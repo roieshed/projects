71ff3d7a6e6bf0ca50be81ce17e432fbef4a1f7e
amitnir29 <54716841+amitnir29@users.noreply.github.com>
2022-02-03 18:24:22 +0200

    --addedClasses

--- wiki_game.py
+++ wiki_game.py
@@ -1,139 +1,195 @@
 #Uses python3
 
+
 import requests
 from bs4 import BeautifulSoup
 import urllib
 import re
-from igraph import * 
+import networkx as nx
+import matplotlib.pyplot as plt
 
-class Wikipidea:
-
-    g = Graph(directed=True)
-    template = 'https://en.wikipedia.org/wiki/'
+class My_graph:
     
+    COLORS = {
+        'start_color' : 'green',
+        'end_color' : 'blue',
+        'normal_color' : 'red',
+        'circle_color' :'yellow',
+        'fail_color' : 'black'
+    }
+
+    def __init__(self):
+
+        self.color_map = []
+        self.circle = False
+        self.G = nx.DiGraph()
+
+    def try_insert_first(self, node, color = COLORS['start_color']):
+
+        if self.G.has_node(node):
+            return False
+        self.G.add_node(node)
+        self.add_color(node, color)
+        return True
+
+    def insert_node(self, node):
+
+        if node is None:
+            return
+        if self.G.has_node(node):
+            self.circle = True
+        else:
+            self.G.add_node(node)
+            self.add_color(node)
+
+    def insert_edge(self, start_node, end_node):
+
+        self.G.add_edge(start_node, end_node)
+        if self._check_circle(start_node, end_node):
+            self.change_colors([start_node, end_node], self.COLORS['circle_color'])
+
+    def _check_circle(self, node1, node2):
+
+        if self.G.has_edge(node1, node2) and self.G.has_edge(node2, node1):
+            return True
+        return False
+
+    def add_color(self, node, color = COLORS['normal_color']):
+
+        self.G.nodes[node]['color'] = color
+
+    def change_color(self, node, color):
+
+        self.G.nodes[node]['color'] = color
+
+    def change_colors(self, nodes : list, color):
+
+        for node in nodes:
+            self.G.nodes[node]['color'] = color
+
+    def print_graph(self):
+        
+        colors_map = [self.G.nodes[x].get('color', 'red') for x in self.G.nodes]
+        nx.draw(self.G ,node_color = colors_map , with_labels=True)
+        plt.show()
+    
+
+    
+
+        
+
+class Wikipedia:
+    
+    RANDOM_PAGE = "https://en.wikipedia.org/wiki/Special:Random"
+    RANDOM_PAGE_TOPIC = ('link', {'rel' : 'canonical'})
+    MAIN_BODY_XPATH = ('div', {'id' : 'mw-content-text'})
+    TEMPLATE = 'https://en.wikipedia.org'
+    END = '/wiki/Philosophy'
+   
+    
+
     def __init__(self):
         pass
-
-    @classmethod
-    def _generate_random_wiki_page(cls) -> str:
     
-        random_page = requests.get("https://en.wikipedia.org/wiki/Special:Random")
-        soup = BeautifulSoup(random_page.content, "html.parser")
-        url = soup.find(class_="firstHeading").text
-        cls.g.add_vertex(url)
-        return  urllib.parse.quote(url)
-
     @classmethod
-    def _reach_philosophy(cls):
+    def parse_main_body(cls, bs4_content):
 
-        def _check_vertex(vertex):
-                try:
-                    x = cls.g.vs.find(vertex).index
-                except:
-                    x = -1
-                return True if x > 0 else False
-            
-
-        def find_first_link(url): # returns the text of the first link
-
-            
-            def _get_paranthesis(text):
-            
-                # check if the link is in between "()", if there are no "()" -> has to be valid
-                query = re.search('\(.*?\)', text, flags = re.DOTALL)
-                if query is None:
-                    return float('inf'), 0
-                return query.start(0), query.end(0)
-
-            def _validate_link(link,  paragraph, range_start, range_end): 
-
-                # all relevent links in wikipedia have a title attr
-                # if link dosent have title or title has been seen -> invalid link
-                if (not link.has_attr('title')) or bool(re.search('/wiki/Wikipedia:', link['href'])) or bool(re.search('/wiki/File:', link['href'])) or not link['href'].startswith("/wiki/"):
-                    return False
-
-                escaped_text = re.escape(link.text)
-                if escaped_text in seen_links:
-
-                    text_indexes = re.finditer(escaped_text,paragraph.text)
-                    for index, match in enumerate(text_indexes):
-                        if index == seen_links[escaped_text]:
-                            link_start = match.start(0)
-                    seen_links[escaped_text] += 1
-
-                else:
-                    
-                    text_indexes = re.search(escaped_text, paragraph.text)
-                    link_start = text_indexes.start(0)
-                    seen_links[escaped_text] = 1
-
-                return  not (range_start < link_start < range_end)
+        return bs4_content.find(*cls.MAIN_BODY_XPATH)
     
-            def _get_html_content(url):
-                
-                url  = cls.template + url 
-                try:
-                    r = requests.get(url)
-                except:
-                    return -1
-                return BeautifulSoup(r.content, 'html.parser')
+    @staticmethod
+    def validate_paragraph(paragraph):
 
-            html_content = _get_html_content(url)
-            if html_content == -1:
-                return -1 
-            main_body = html_content.find('div', {'id' : 'mw-content-text'})
-            if main_body is None:
-                return -1
-            
-            # iterates through all links in all paragraphs and returns the first link that is by the rules
-            for paragraph in main_body.find_all('p'):
-                seen_links = {} ## keeps track if ive seen links
-                open_paranthesis, close_paranthesis = _get_paranthesis(paragraph.text)
-                for link_number, link in enumerate(paragraph.find_all(href=True)):
-                    ## change it so i can get the index during the findall
-                    if _validate_link(link, paragraph, open_paranthesis, close_paranthesis):
-                        return link['href'][6:]
-            
-            # reached a page with no links
-            print('failed')
-            return -1
-            
-        current_page = cls._generate_random_wiki_page()
-
-        while current_page != 'Philosophy':  
-            
-            next_page = find_first_link(current_page) #function returns -1 if there are no links
-            if next_page == -1:
-                break
-            
-            next_vertex, current_vertex = urllib.parse.unquote(next_page), urllib.parse.unquote(current_page)
-            if _check_vertex(next_vertex):
-                cls.g.add_edge(current_vertex, next_vertex)
-                break
-            
-            cls.g.add_vertex(next_vertex)
-            cls.g.add_edge(current_vertex, next_vertex)
-            current_page = next_page 
-        
+        if paragraph.find_parent('table'):
+            return False
+        else:
+            return True
 
     @classmethod
-    def print_philosophy_graph(cls):
-        
-        cls.g.vs["label"] = cls.g.vs['name']
-        color = {"Philosophy" : "blue"}
-        cls.g.vs['color'] = [color.get(name, 'red') for name in cls.g.vs['name']]
-        plot(cls.g, vertex_color = cls.g.vs['color'], bbox = (1200,800))
+    def get_next_topic_href(cls, topic):
 
+        content = cls.parse_url_to_bs4(f'{cls.TEMPLATE}{topic}')
+        main_body = cls.parse_main_body(content)
+        for paragraph in cls.find_all_paragraphs(main_body):
+                if cls.validate_paragraph(paragraph):
+                    for link in cls.find_all_links(paragraph):
+                        if cls.validate_link(link):
+                            return link['href']
+        
+        return None
+
+    @classmethod
+    def get_random_page_href(cls):
+
+        soup = cls.parse_url_to_bs4(cls.RANDOM_PAGE)
+        topic = soup.find(*cls.RANDOM_PAGE_TOPIC)['href']
+        return topic.split(cls.TEMPLATE)[-1]
+
+    @staticmethod
+    def parse_url_to_bs4(url):
+
+        try:
+            r = requests.get(url)   
+        except:
+            raise "failed to get url"
+        return BeautifulSoup(r.content, 'html.parser')
+    
+    @staticmethod
+    def validate_link(link):
+
+        edge_cases = ['/wiki/Geographic_coordinate_system']
+        parantheis = []
+        for sibling in link.previous_siblings:
+            parantheis += re.findall('[\(\)]', sibling.text)
+        
+        valid = len(parantheis) % 2 == 0 and not bool(re.match('.*?:.*?', link['href'])) and link['href'] not in edge_cases
+        return valid
+
+    @staticmethod
+    def find_all_paragraphs(bs4_content):
+
+        paragraphs = bs4_content.find_all('p')
+        return paragraphs
+
+    @staticmethod
+    def find_all_links(paragraph):
+
+        links = paragraph.find_all('a', href=lambda x: x is not None and x.startswith("/wiki/")) 
+        return links
+
+    @staticmethod
+    def parse_wiki_href_to_node(wiki_href):
+
+        if wiki_href is None:
+            return wiki_href
+        return wiki_href.split('/')[-1]
+
+def build_wiki_graph():
+    ## this function feels very disorginized to me, how would you reccomend to do it?
+    g.circle = False
+    topic = wiki.get_random_page_href()
+    if g.try_insert_first(wiki.parse_wiki_href_to_node(topic)):
+        while topic != wiki.END and not g.circle:
+    
+            current = topic
+            topic = wiki.get_next_topic_href(topic)
+            current_node = wiki.parse_wiki_href_to_node(current)
+            next_node = wiki.parse_wiki_href_to_node(topic)
+            g.insert_node(next_node)
+            g.insert_edge(current_node, next_node)
+
+            if next_node == None:
+                g.change_color(current_node, g.COLORS['fail_color'])
+            if topic == wiki.END:
+                g.change_color(next_node, g.COLORS['end_color'])
 
 if __name__ == '__main__':
 
-    wiki = Wikipidea()
+    g = My_graph()
+    wiki = Wikipedia
     for i in range(int(input())):
-        Wikipidea._reach_philosophy()
-        print(f"managed cycle {i}")
-
-    Wikipidea.print_philosophy_graph()
-    
+        build_wiki_graph()
+        print(f'managed cycle {i}')
+    g.print_graph()
 
     
     
@@ -141,3 +197,9 @@ if __name__ == '__main__':
     
         
     
+
+
+
+
+
+
